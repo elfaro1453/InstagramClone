c3f0e53d7681b496bbf037424450205ce55fa1bd
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2020-08-13 08:22:56 +0700

    13/08/2020

--- app/src/main/java/idn/fahru/instagramclone/MainActivity.kt
+++ app/src/main/java/idn/fahru/instagramclone/MainActivity.kt
@@ -1,11 +1,32 @@
 package idn.fahru.instagramclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import idn.fahru.instagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // mendeklarasikan binding viewBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // viewbinding inflater
+        val inflater = layoutInflater
+        // set viewbinding untuk activity_main.xml
+        binding = ActivityMainBinding.inflate(inflater)
+        // setContentView diisi dengan binding.root
+        setContentView(binding.root)
+
+        // Definisikan id NavHostFragment yang ada di activity_main.xml,
+        // idnya itu fragment_container
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        // navView itu adalah id dari bottomNavigation
+        binding.navView.setupWithNavController(navController)
     }
 }
