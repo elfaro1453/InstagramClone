f422ccc23fd7b6869922c2d232fa7eb1a66d7b0d
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2020-08-18 11:25:09 +0700

    update 18/08/2020

--- app/src/main/java/idn/fahru/instagramclone/RegisterActivity.kt
+++ app/src/main/java/idn/fahru/instagramclone/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package idn.fahru.instagramclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import idn.fahru.instagramclone.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+
+    // buat variabel dialog
+    private lateinit var dialog: LoadingDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +20,9 @@ class RegisterActivity : AppCompatActivity() {
         val inflater = layoutInflater
         binding = ActivityRegisterBinding.inflate(inflater)
         setContentView(binding.root)
+
+        // buat dulu dialog progressdialog sebagai efek loading
+        dialog = LoadingDialog(this)
 
         binding.run {
             // tambahkan setonclicklistener pada tombol btnSignin
@@ -37,7 +46,7 @@ class RegisterActivity : AppCompatActivity() {
         Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show()
     }
 
-    // buat fungsi createAccount()
+    // buat fungsi private createAccount()
     private fun createAccount() {
         // gunakan binding.run karena kita akan akses view di layout
         binding.run {
@@ -51,18 +60,22 @@ class RegisterActivity : AppCompatActivity() {
             // return digunakan untuk mengakhiri jalannya fungsi
             if (fullName.isEmpty()) {
                 showToast("Fullname harus diisi !")
+                // return digunakan untuk menyelesaikan fungsi createAccount() lebih awal
                 return
             }
             if (emailUser.isEmpty()) {
                 showToast("Email harus diisi !")
+                // return digunakan untuk menyelesaikan fungsi createAccount() lebih awal
                 return
             }
             if (userName.isEmpty()) {
                 showToast("Username harus diisi !")
+                // return digunakan untuk menyelesaikan fungsi createAccount() lebih awal
                 return
             }
             if (passWord.isEmpty()) {
                 showToast("Password harus diisi !")
+                // return digunakan untuk menyelesaikan fungsi createAccount() lebih awal
                 return
             }
             if (!emailUser.isEmailValid()) {
@@ -77,7 +90,62 @@ class RegisterActivity : AppCompatActivity() {
                 return
             }
 
-            showToast("Semua kolom telah diisi !")
+            // munculkan loading sebelum menyimpan data ke firebase
+            dialog.startLoadingDialog()
+
+            // sambungkan ke Firebase Auth
+            val mAuth = FirebaseAuth.getInstance()
+            mAuth.createUserWithEmailAndPassword(emailUser, passWord)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // panggil fungsi saveUserInfo
+                        // untuk menyimpan data user seperti fullname dan username
+                        saveUserInfo(fullName, userName, emailUser)
+                    } else {
+                        // jika gagal membuat user maka tampilkan toast berisi errornya
+                        val message = task.exception
+                        showToast(message.toString())
+                        mAuth.signOut()
+                        // jika gagal, loading ditutup menggunakan dismissDialog()
+                        dialog.dismissDialog()
+                    }
+                }
         }
     }
+
+    // buat fungsi saveUserInfo()
+    private fun saveUserInfo(fullName: String, userName: String, emailUser: String) {
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val userRef = FirebaseDatabase.getInstance().reference.child("users")
+        val userMap = HashMap<String, Any>()
+        userMap["uid"] = currentUserId
+        userMap["fullname"] = fullName
+        userMap["username"] = userName
+        userMap["email"] = emailUser
+        userMap["Bio"] = ""
+        userMap["image"] = ""
+
+        // fungsi di bawah ini untuk memasukkan data ke dalam database firebase
+        userRef.child(currentUserId).setValue(userMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) { // jika berhasil update firebase
+                    // jika sukses tutup dialog
+                    dialog.dismissDialog()
+                    showToast("Akun Sudah dibuat")
+                    // buat intent yang menuju mainactivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    // tambahkan flag activity clear task untuk nonaktifkan tombol back
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // jika gagal tutup dialog
+                    dialog.dismissDialog()
+                    val message = task.exception.toString()
+                    showToast(message)
+                    FirebaseAuth.getInstance().signOut()
+                }
+            }
+    }
+
 }
