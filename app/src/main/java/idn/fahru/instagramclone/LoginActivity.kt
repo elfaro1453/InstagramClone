2bc9e5e73f9574ec263ede9745d062bb11146e27
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2020-08-19 10:39:21 +0700

    update 19/08/2020

--- app/src/main/java/idn/fahru/instagramclone/LoginActivity.kt
+++ app/src/main/java/idn/fahru/instagramclone/LoginActivity.kt
@@ -11,6 +11,9 @@ class LoginActivity : AppCompatActivity() {
     // VIEWBINDING untuk Activity_Login.xml
     private lateinit var binding: ActivityLoginBinding
 
+    // buat variabel loginDialog
+    private lateinit var loginDialog: LoadingDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,6 +22,9 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(inflater)
         setContentView(binding.root)
         // deklarasi VIEWBINDING selesai
+
+        // deklarasikan loading dialog
+        loginDialog = LoadingDialog(this)
 
         binding.run {
             // menambahkan click pada btnsignup
@@ -60,23 +66,49 @@ class LoginActivity : AppCompatActivity() {
         // jika email dan password sudah sesuai
         // buat variabel untuk menghubungkan ke Firebase
         val mAuth = FirebaseAuth.getInstance()
+
+        // tampilkan loading terlebih dahulu sebelum ke firebase
+        loginDialog.startLoadingDialog()
+
         // masuk ke firebase dengan email dan password
         mAuth.signInWithEmailAndPassword(email, passWord)
             .addOnCompleteListener { task ->
                 // jika berhasil login
                 if (task.isSuccessful) {
+                    // tutup loading
+                    loginDialog.dismissDialog()
                     // lanjut intent ke MainActivity
                     val intent = Intent(this, MainActivity::class.java)
+                    // addFlag menambahkan opsi pada intent
+                    // Flag FLAG_ACTIVITY_CLEAR_TASK digunakan untuk menonaktifkan tombol back
+                    // Flag FLAG_ACTIVITY_NEW_TASK digunakan untuk membuat activity baru
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    // mulai intent
                     startActivity(intent)
+                    // tutup activity login dengan finish()
                     finish()
                 } else { // jika tidak berhasil login
                     // Tampilkan Toast pesan error login
                     val message = task.exception.toString()
                     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                    // sign Out untuk keluar dari firebase
                     mAuth.signOut()
+                    // tutup loading
+                    loginDialog.dismissDialog()
                 }
             }
     }
 
+    // fungsi onStart dijalankan setelah onCreate
+    override fun onStart() {
+        super.onStart()
+
+        // Jika user tidak null atau ada maka lanjut ke MainActivity
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
